4c46ffa4bd2cc2c6071d89ff4923612d9f93bd6e
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2025-12-31 12:25:48 +0100

    Implement a PostCard Component 2

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -3,28 +3,11 @@ import Header from '@/components/layout/Header';
 import PostCard from '@/components/common/PostCard';
 import { type PostProps } from '@/interfaces';
 
-const PostsPage = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostPageProps {
+  posts: PostProps[];
+}
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/posts'
-      );
-      const data = await response.json();
-
-      const formattedPosts: PostProps[] = data
-        .slice(0, 10)
-        .map((post: any) => ({
-          title: post.title,
-          contents: post.body,
-          userId: post.userId,
-        }));
-      setPosts(formattedPosts);
-    };
-    fetchPosts();
-  }, []);
-
+const PostsPage = ({ posts }: PostPageProps) => {
   return (
     <>
       <Header />
@@ -41,6 +24,24 @@ const PostsPage = () => {
       </div>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  const data = await response.json();
+
+  const posts = data.slice(0, 10).map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userID,
+  }));
+
+  return {
+    props: {
+      posts
+    }
+  }
 };
 
 export default PostsPage;
