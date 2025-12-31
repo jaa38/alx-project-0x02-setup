c40ccf4d691e09634f083c53ca82e50ea710391f
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2025-12-31 23:44:02 +0100

    Build a UserCard Component 3

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -25,7 +25,7 @@ const UsersPage = ({ users }: UsersPageProps) => {
   );
 };
 
-export const getStaticProps = async () => {
+export async function getStaticProps() {
   const respone = await fetch('https://jsonplaceholder.typicode.com/users');
 
   const data = await respone.json();
